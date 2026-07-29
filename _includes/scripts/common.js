b2e80ad2fa9f4c0e8310d776928df1c19a69e5e7
Mihua <2602977906@qq.com>
2026-07-29 17:42:47 +0800

    名誉维护者已更新

--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -1,5 +1,10 @@
 (function () {
   var $root = document.getElementsByClassName('root')[0];
+  var currentYear = new Date().getFullYear();
+  var $currentYears = document.querySelectorAll('.js-current-year');
+  for (var i = 0; i < $currentYears.length; i++) {
+    $currentYears[i].textContent = currentYear;
+  }
   if (window.hasEvent('touchstart')) {
     $root.dataset.isTouch = true;
     document.addEventListener('touchstart', function(){}, false);
